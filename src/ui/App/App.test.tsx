991e68372cd9d9a6d57e685ced09b9db5e88d5d4
Chris Wagner <mail@cswagner.io>
2023-11-19 20:33:32 -0500

    Use implicit Jest globals

--- src/ui/App/App.test.tsx
+++ src/ui/App/App.test.tsx
@@ -1,11 +1,7 @@
-import 'react-native'
 import React from 'react'
-import { App } from '.'
-
-// Note: import explicitly to use the types shiped with jest.
-import { it } from '@jest/globals'
-
+import 'react-native'
 import { render } from '@testing-library/react-native'
+import { App } from '.'
 
 it('renders correctly', () => {
   render(<App />)
