3a4e39a6e741c08a87af54e26412b2a837c9495a
Chris Wagner <mail@cswagner.io>
2023-11-19 20:31:10 -0500

    Use @testing-library/react-native

--- src/ui/App/App.test.tsx
+++ src/ui/App/App.test.tsx
@@ -5,9 +5,8 @@ import { App } from '.'
 // Note: import explicitly to use the types shiped with jest.
 import { it } from '@jest/globals'
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer'
+import { render } from '@testing-library/react-native'
 
 it('renders correctly', () => {
-  renderer.create(<App />)
+  render(<App />)
 })
